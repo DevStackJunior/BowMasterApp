623a0c79cf74abd5c0b2dc65dcea3924895d9c2a
JuniorDev <pv00wyv@eduvaud.ch>
2025-03-14 18:00:03 +0100

    Feat (Switching Player) : Game conditions to stop & continue
    Creating the necessary environment to loop over and over again as long as a player is still standing.
    Passing through adverserial player each time ball is launched.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,8 @@
 *Description : 
 */
 using System.Numerics;
+using static ConsoleApp1.ItemsDisplay;
+using static System.Formats.Asn1.AsnWriter;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
@@ -21,17 +23,52 @@ namespace ConsoleApp1
 
             Player playerTwo = new Player(2);
 
+            //ball starts to move from above the player 1 head
             var startPosition = new Vector2(3, GameBoard.ScreenHeight - 6);
 
-            while (playerOne.LivesOwned > 0 || playerTwo.LivesOwned > 0)
-            {
-                Ball ball = new Ball(startPosition, 1f, 1);
+            //ball starts to move from above the player 2 head
+            var startPositionP2 = new Vector2(GameBoard.ScreenWidth - 4, GameBoard.ScreenHeight - 6);
+            
+            Ball ballPlayer1 = new Ball(startPosition, 1f, 1);
 
+            Ball ballPlayer2 = new Ball(startPositionP2, 1f, 2);
+
+            int amountOfTries = 1;
+
+            while (playerOne.LivesOwned > 0 && playerTwo.LivesOwned > 0)
+            {
                 GameBoard.GameBoardDisplay();
-                float score = await playerOne.IsSetPowerLaunch(playerOne);
-                var angle = await playerOne.AnimatedBowPlayer1();
-                await ball.LaunchAtTarget(40f, score, playerTwo);
-                GameBoard.GameBoardDisplay();
+                
+                
+                //player 1 selected
+                if(amountOfTries % 2 != 0)
+                {
+                    float score = await playerOne.IsSetPowerLaunch(playerOne);
+                    var angle1 = await playerOne.AnimatedBowPlayer();
+                    await ballPlayer1.LaunchAtTarget(angle1, score, playerOne);
+                    //refreshing the state of displayed items on player 2 side
+                    GameBoard.GameBoardDisplay();
+                }
+                //player 2 selected
+                else if (amountOfTries % 2 == 0)
+                {
+                    float score2 = await playerTwo.IsSetPowerLaunch(playerTwo);
+                    var angle = await playerTwo.AnimatedBowPlayer();
+                    await ballPlayer2.LaunchAtTarget(angle, score2, playerTwo);
+                    //refreshing the state of displayed items on player 1 side
+                    GameBoard.GameBoardDisplay();
+                }                
+                amountOfTries++;
+            }
+            if(playerOne.LivesOwned <= 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Player N°2 wins! {playerTwo.LivesOwned} remaining lives. You had {amountOfTries} attempts");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Player N°1 wins! {playerOne.LivesOwned} remaining lives. You had {amountOfTries} attempts");
             }
             // Create a new ball
             // Player playerTwo = new Player(2);
