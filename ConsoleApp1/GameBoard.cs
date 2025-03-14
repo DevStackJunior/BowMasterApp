04421b826ec76e7d7322b6bd5431839d6f1635d1
JuniorDev <pv00wyv@eduvaud.ch>
2025-03-14 11:51:59 +0100

    Fix(GameBoard) : GamePlay Console
    - readapting the size of game console to 150x40 instead of 40x50.
    - Correcting functions displaying players & towers within game console (not appropriate class).
    - Displaying whole dashboard within white background

--- ConsoleApp1/GameBoard.cs
+++ ConsoleApp1/GameBoard.cs
@@ -1,223 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
+﻿/* ETML
+* Author : VPA
+* Date : 11.03.2025
+* Description : 
+*/
 using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using static ConsoleApp1.ItemsDisplay;
 
 namespace ConsoleApp1
 {
     /// <summary>
     /// Displays the gameboard background color black amongst whole terminal window
     /// </summary>
-    public class GameBoard
+    public static class GameBoard
     {
         /// <summary>
         /// Establish the screen width of console display in which game will be rendered
         /// </summary>
-        public int ScreenWidth
-        {
-            get;
-            private set;
-        }
+        public static int ScreenWidth { get; private set; } = 150;
 
         /// <summary>
         /// Establish the screen height of console display in which game will be rendered
         /// </summary>
-        public int ScreenHeight
-        {
-            get;
-            private set;
-        }
-
-        //Managing item colored & non-colored displayed through game board 
-        public Pixel[,] Display
-        {
-            get;
-            private set;
-        }
+        public static int ScreenHeight { get; private set; } = 40;
 
         /// <summary>
-        /// Arc Player Reversible according to the player number (1 or 2)
-        /// NECESSARY TO ADAPT TO CONSOLE SIZE GAMEPLAY AS IN DRAWTOWER() METHOD
+        /// Managing item colored 
         /// </summary>
-        /// <param name="player">Player1/2 | Properties</param>
-        public void DrawBowPlayer(Pixel[,] display, Player player)
-        {
-            // The radius of the circle
-            int radius = 10;
-            // Arc Player A | The width center X coordinate (in the game console grid)
-            int xAnglePositionPlayerA = 8;
-            // Arc Player B | The width center X coordinate (in the game console grid)
-            int xAnglePositionPlayerB = 56;
-            // Arc Player A & B | The center Y coordinate (in the game console grid)
-            int yAnglePosition = 12;
-            // Space between points (in pixels)
-            int spacing = 5;
-
-            // Number of steps to make the arc smoother or jagged
-            int steps = 50;
-
-            if (player.PlayerNumber == 1)
-            {
-                // Forward arc: Loop through angles to plot the arc (from 0 to 90 degrees)
-                for (int i = 0; i <= steps; i++)
-                {
-                    // Angle in radians for forward arc
-                    // From 0 to 90 degrees
-                    double angle = (Math.PI / 2) * i / steps;
-
-                    // Calculate the x and y position on the circle
-                    int x = (int)(xAnglePositionPlayerA + radius * Math.Cos(angle));
-                    // Invert Y to match console coordinate system
-                    int y = (int)(yAnglePosition - radius * Math.Sin(angle));
-
-                    // Space out the points by 5 pixels (using a simple loop for spacing)
-                    for (int s = 0; s < spacing; s++)
-                    {
-                        Console.SetCursorPosition(x + s, y);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("*");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        display[y, x].PixelColor = ConsoleColor.Yellow;
-                    }
-                }
-            }
-            else if (player.PlayerNumber == 2)
-            {
-                // Reverse arc: Loop through angles to plot the arc in reverse (from 90 to 0 degrees)
-                for (int i = steps; i >= 0; i--)
-                {
-                    // Angle in radians for reverse arc
-                    // From 90 to 0 degrees
-                    double angle = (Math.PI / 2) * i / steps;
-
-                    // Calculate the x and y position on the circle
-                    int x = (int)(xAnglePositionPlayerB + radius * Math.Cos(angle));
-                    int y = (int)(yAnglePosition - radius * Math.Sin(angle)); // Invert Y to match console coordinate system
-
-                    // Space out the points by 5 pixels
-                    for (int s = 0; s < spacing; s++)
-                    {
-                        Console.SetCursorPosition(x + s, y);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("*");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        //display[y, x].PixelColor = ConsoleColor.Yellow;
-                    }
-                }
-            }
-        }
-
+        public static Pixel[,] Display { get; private set; }
 
         /// <summary>
-        /// Drawing towers of each player with equal space between console external borders and each tower 
-        /// Creates 2 different ways : 
-        /// If player A : draws the tower from top to bottom and from left to right
-        /// If player B : draws the tower from top to bottom and from right to left
+        /// Initializes the gameboard's dimensions and display settings.
         /// </summary>
-        /// <param name="display"> array of pixels (2nd property the console grid can take) </param>
-        /// <param name="player">  </param>
-        public void DrawTower(Pixel[,] display, Player player)
+        static GameBoard()
         {
-            int xTowerPosition = 5;
+            Console.CursorVisible = false;
 
-            // First condition establish the tower A drawing
-            // The Grid Console Drawing is by default drawing from TOP to BOTTOM -> Decrementing is necessary in y axis to gain height on tower side draw
-            // LEFT to RIGHT
-            if (player.PlayerNumber == 1)
-            {
-                //"display.GetLength(0)" catches the total height (lines accumulation) of the window console as reference point
-                for (int y = display.GetLength(0) - 1; y < display.GetLength(0) - player.TowerOwned.HeightTower; y--)
-                {
-                    //"display.GetLength(1)" catches the total width (columns accumulation) of the window console as reference point
-                    for (int x = 0 + xTowerPosition; x < player.TowerOwned.WidthTower + xTowerPosition; x++)
-                    {
-                        display[y, x].PixelColor = ConsoleColor.White;
-                    }
-                }
-            }
-            else if (player.PlayerNumber == 2)
-            {
-                //"display.GetLength(0)" catches the total height of the window console as reference point
-                for (int y = display.GetLength(0) - 1; y < display.GetLength(0) - player.TowerOwned.HeightTower; y--)
-                {
-                    //"display.GetLength(1)" catches the total width of the window console as reference point
-                    for (int x = (display.GetLength(1) - xTowerPosition); x > display.GetLength(1) - player.TowerOwned.WidthTower + xTowerPosition; x--)
-                    {
-                        display[y, x].PixelColor = ConsoleColor.White;
-                    }
-                }
-            }
+            Console.WindowWidth = ScreenWidth;
+            Console.WindowHeight = ScreenHeight;
+            Console.BufferWidth = ScreenWidth;
+            Console.BufferHeight = ScreenHeight;
 
-        }
-
-        /// <summary>
-        /// Constructor GameBoard delivers once only, the limit properties of game display
-        /// </summary>
-        public GameBoard()
-        {
-            Console.WindowWidth = 40;
-            Console.WindowHeight = 50;
-
-            Console.BufferWidth = 40;
-            Console.BufferHeight = 50;
-
-            ScreenWidth = 40;
-            ScreenHeight = 50;
-
-            //Create the table of game console
+            // Create the table of game console
             Display = new Pixel[ScreenHeight, ScreenWidth];
 
-            //Init the table of game console
-            //Prints the gameboard background in black
+            // Init the table of game console
             for (int y = 0; y < ScreenHeight - 1; y++)
             {
                 for (int x = 0; x < ScreenWidth - 1; x++)
                 {
-
-                    //Print the whole table in white pixel side color 
-                    Pixel currentPixel = new Pixel
-                    {
-                        PixelColor = ConsoleColor.White,
-                    };
-                    Display[y, x] = currentPixel;
+                    Display[y, x] = new Pixel { PixelColor = ConsoleColor.White };
                 }
             }
             GameBoardDisplay();
-
         }
+
         /// <summary>
-        /// Display graphically the table 
+        /// Display graphically the table of the game console overview 
         /// </summary>
-        public void GameBoardDisplay()
+        public static void GameBoardDisplay()
         {
             Console.SetWindowSize(ScreenWidth, ScreenHeight);
+            Console.Clear();
 
             for (int y = 0; y < ScreenHeight - 1; y++)
             {
                 for (int x = 0; x < ScreenWidth - 1; x++)
                 {
                     Console.SetCursorPosition(x, y);
-                    //If current pixel returns no value (char) so prints pixelcolor
                     if (Display[y, x].PixelColor != null)
                     {
-                        //part du principe que ne sera pas null et qu'une valeur sera de toute façon existente (valeur acquise de couleur de pixel)
                         Console.BackgroundColor = Display[y, x].PixelColor.Value;
                         Console.Write(" ");
                     }
-                    else
-                    //Else prints PixelASCII (char) value | displays items
+                    else if (Display[y, x].PixelASCII == '♥')
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(Display[y, x].PixelASCII);
+                    }
+                    else if (Display[y, x].PixelASCII == '*')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(Display[y, x].PixelASCII);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
                         Console.Write(Display[y, x].PixelASCII);
                     }
                 }
-                //Forces the pass to the next line generation of the grid game console
                 Console.WriteLine();
             }
+        }
+
+        /// <summary>
+        /// Checks if the ball has collided with an obstacle & game console screen borders
+        /// </summary>
+        public static bool CheckCollision(Vector2 pos, Player player)
+        {
+            int x = (int)pos.X;
+            int y = (int)pos.Y;
+
+            if (x < 0 || x >= ScreenWidth || y < 0 || y >= ScreenHeight)
+                return true;
+
+            if (Display[y, x] != null && Display[y, x].PixelColor != null && Display[y, x].PixelColor == ConsoleColor.Gray)
+            {
+                if (player.TowerOwned.HitStone(x, y))
+                {
+                    Display[y, x].PixelColor = ConsoleColor.White;
+                    return true;
+                }
+            }
+            else if (Display[y, x] != null && Display[y, x].PixelASCII != null && Display[y, x].PixelASCII != '*')
+            {
+                for (int i = 0; i < player.PlayerMembers.GetLength(0); i++)
+                {
+                    for (int j = 0; j < player.PlayerMembers.GetLength(1); j++)
+                    {
+                        if (player.PlayerMembers[i, j] == Display[y, x].PixelASCII)
+                        {
+                            player.PlayerIsHit();
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
